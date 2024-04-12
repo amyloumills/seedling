9c78a5cc584772e2e992ee68c15b6a9b11352a02
Amy Miller <amyloumiller123@gmail.com>
2024-04-12 23:42:00 +0100

    first push

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Welcome to Seedling 🌱</h1>
+  <p>Identify and add your plants</p>
+</template>
+
+<style scoped>
+h1 {
+  color: #9ccc65;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-weight: 200;
+}
+p {
+  font-weight: 200;
+}
+</style>
